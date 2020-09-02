6d089dd98ae29615c31949e0bdeefadde053c85a
amitpal.it89 <amitpal.it89@gmail.com>
2020-09-02 21:53:44 +0530

    Initial

---
+++ musicalog-api/Album.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace musicalog_api
+{
+    public class Album
+    {
+        public string Name { get; set; }
+
+        public string Artist { get; set; }
+    }
+}
---
+++ musicalog-api/Controllers/AlbumController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace musicalog_api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class AlbumController : ControllerBase
+    {
+        
+        [HttpPost]
+        [Route("create")]
+        public bool CreateAlbum(Album album)
+        {
+            return true;
+        }
+
+        [HttpGet]
+        [Route("/{id?}")]
+        public Album GetAlbum(int id)
+        {
+            return new Album
+            {
+                Artist = "Amit",
+                Name = "firstAlbum"
+            };
+        }
+    }
+}
